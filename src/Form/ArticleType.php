10f7a402fc8f6ab3e0f040cca62f3236ced6283e
elitepaysages@gmail.com <elitepaysages@gmail.com>
2021-12-07 17:01:22 +0100

    ajout de nouveaux fichiers

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -5,7 +5,11 @@ namespace App\Form;
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
@@ -16,22 +20,52 @@ class ArticleType extends AbstractType
         $builder
             ->add('titre', TextType::class, [
                 'label' => 'Titre de l\'article :',
+                'required' => false,
                 'attr' => [
                     'placeholder' => "Saisir le titre de l'article",
+                ],
+                'constraints' => [
+                    new Length([
+                        'min' => 10,
+                        'max' => 50,
+                        'minMessage' => "Titre trop court (min 10 caractères)",
+                        'maxMessage' => "Titre trop long (max 50 caractères)"
+                    ]),
+                    New NotBlank([
+                        'message' => "Merci de saisir un autre titre"
+                    ])
                 ]
             ])
             ->add('contenu', TextareaType::class, [
                 'label' => 'Contenu de l\'article :',
+                'required' => false,
                 'attr' => [
                     'placeholder' =>"Saisir le contenue de l'article",
                     'rows' => 10
+                ],
+                'constraints' =>[
+                New NotBlank([
+                    'message' => "Merci de saisir un contenu d'article"
+                ])
                 ]
             ])
-            ->add('photo', TextType::class, [
-                'label' => 'URL de la photo :',
-                'attr' => [
-                    'placeholder' => "Saisir l'URL de la photo"
-                ]         
+            ->add('photo', FileType::class, [
+                'label' => 'Uploader une photo :',
+                "mapped" => true, //
+                'required' => false,
+                "data_class" => null,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/jpg'
+                        ],
+                        "mimeTypesMessage" => 'Format autorisés : jpg/png/jpeg.'
+                    ]),
+                ]
+                         
              ])
         ;
     }
